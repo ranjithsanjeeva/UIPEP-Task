212c95dd71aaeba35645de1b6a2d6f2e4f3e8bd9
Ranjith <ranjithsanjeeva@gmail.com>
2020-08-01 16:53:19 +0530

    task3 get odds list is done

---
+++ get_odds_list.js
@@ -0,0 +1,25 @@
+function get_odds_list(count) {
+  
+    /*
+  This method returns a list of the first 'count' odd numbers in descending
+  order. e.g count = 3 should return [5,3,1]
+  */
+ let array=[];
+ let j=0;
+ for(let i=0;i<(2*count);i++)
+ {
+     if(i%2!==0)
+     {
+        array[count-j-1]=i;
+        j++;
+     }
+ }
+console.log(array);
+
+}
+
+console.assert([1],get_odds_list(1))
+console.assert([], get_odds_list(0))
+console.assert([5,3,1],get_odds_list(3))
+console.assert([9,7,5,3,1], get_odds_list(5))
+
