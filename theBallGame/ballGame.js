e20215168c751da29c6d456c1896fc481e2e7e0b
Ranjith <ranjithsanjeeva@gmail.com>
2020-08-06 20:44:46 +0530

    now ball is moving and created more than 1 balls now need to fix issues

--- theBallGame/ballGame.js
+++ theBallGame/ballGame.js
@@ -1,33 +1,76 @@
 var canvas = document.getElementById("img1");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width=window.innerWidth-15;
+canvas.height=window.innerHeight-140;
 canvas.addEventListener('click',drawCircle,false);
-
+let running = false;
+let c=canvas.getContext("2d");
+circle = [];
 function drawCircle(event){
+
+
+     function Circle(x,y,dx,dy,radius,randomColor){
+        this.x=x;
+        this.y=y;
+        this.dx=dx;
+        this.dy=dy;
+        this.radius=radius;
+        this.randomColor=randomColor
+        this.draw=function(){
+            c.fillStyle=this.randomColor;
+            c.beginPath();
+            c.arc(this.x,(this.y-130),this.radius,0,2*Math.PI)
+            c.stroke();
+            c.fill();
+            this.update();
+        }
+
+        this.update= function(){
+            if(this.x+this.radius > innerWidth-15|| this.x-this.radius<0)
+            {
+                this.dx=-this.dx
+            }
+            if(this.y+this.radius > innerHeight-10||this.y-this.radius<140)
+            {
+                this.dy=-this.dy
+            }
+
+            this.x += this.dx;
+            this.y += this.dy;
+            // console.log(this.x)
+            // console.log(this.x)
+            
+       }
+     }
+
+
+
     console.log("outside1")
-    var canvas = document.getElementById("img1");
-   
-    let c=canvas.getContext("2d");
     let randomColor = "#" + Math.floor(Math.random() * 25542195).toString(16);
-    c.fillStyle=randomColor;
-    c.beginPath();
+    let x=event.clientX;
+    let y=event.clientY;
+    let dx=2*Math.random();
+    let dy=1;
+    let radius=20
+    circle.push(new Circle(x,y,dx,dy,radius,randomColor))
+
+    function animate(){
+        //console.log("outside2")
+        requestAnimationFrame(animate);
     
-   
-    c.arc(event.clientX,(event.clientY-130),20,0,2*Math.PI)
-    console.log(event.clientX)
-    console.log(event.clientY)
-    c.stroke();
-    c.fill();
-    console.log("outside3")
+       c.clearRect(0,0,innerWidth,innerHeight);
+        //console.log("outside3")
+        for(let i=0;i<circle.length;i++) {
+            // circle[i].draw();
+            window.setTimeout(circle[i].draw(), 1000);
+           // console.log(circle);
+        }
+        console.log("outside4")
+      //  animate();
+    }
+    
+    animate();
+
 }
-
-
-
-
-
-
-
-
 
 
 console.log("outside")
